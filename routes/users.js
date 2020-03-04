54fa7e721ab92f9393462d6a87bb00c1150f4786
msheezi <marshall.randolph@gmail.com>
2020-03-03 16:20:55 -0800

    Add index route for users, add join for getting user photos to user object before send

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,79 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const UserPicture = require('../models/userPicture')
 
 
 // index view
-router.get("/", (req, res) => {
-  User.find().then(users => {
-    res.json(users);
-  });
-});
+// router.get("/", (req, res) => {
+//   let profileUsers = {}
+//   User.find().then(users => {
+//     // returns array of objects, want to add a key to user object
+//     // 
+//     users.forEach(obj => {
+//       //obj._doc
+
+//       let pictureURLs = []
+//       obj._doc["pictures"] = []
+//       UserPicture.find({userId: obj._doc._id}, {pictureUrl: 1, _id:0})
+//       .then(pictureArray => {
+//         console.log(pictureArray)
+//         pictureArray.forEach(pictureObject => {
+//           // console.log("Picture Object:", pictureObject)
+//           // console.log("Picture Object._doc:",pictureObject._doc)
+//           // pictureURLs.push(pictureObject._doc["pictureUrl"])
+//           obj._doc["pictures"].push(pictureObject["pictureUrl"])
+//         })
+//         // obj._doc[pictureArray] = pictureURLs
+       
+//       })
+//     })
+//     res.json(users);
+//   });
+// });
+
+router.get("/", (req,res)=>{
+  // make sure in the aggregations, you used the mongo table name
+  User.aggregate([
+    // {$project: {
+    //   "pictureURLS":{
+    //     "pictureUrl": 1
+    //   }
+    // }},
+    {"$addFields": {"id": {"$toString": "$_id"}}}, // this stringifies the _id field so you can match on it
+    {$lookup: {
+      "from": "userpictures",
+      "localField": "id",
+        "foreignField": "userId",
+        "as": "pictureURLs"
+    }
+  },
+  // {$project: {"pictureURLS": {"pictureUrl": 1}}}
+])
+    
+  // User.find().then(users => {
+  //   users.aggregate([{
+  //     $lookup: {
+  //       from: UserPicture.find(), 
+  //       localField: "_id",
+  //       foreignField: "userId",
+  //       as: "pictureURLs"
+  //     }
+  //   }])
+  // })
+  // users.aggregate([{$lookup: {
+  //     from: userPictures,
+  //     localField: "_id", 
+  //     foreignField: "userId",
+  //     as: "pictureURLs"
+  //   }}])
+    .then(combinedUsers => res.json(combinedUsers))
+  })
+  
+
+
+
+
 
 // create new user
 router.post("/", (req, res) => {
