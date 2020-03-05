f5ec8412443c48cda215d9b5522bf86baec1ac54
msheezi <marshall.randolph@gmail.com>
2020-03-05 02:15:44 -0800

    More boilerplate, add navbar

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import {AuthRoute, ProtectedRoute} from '../util/route_util'
 import {Switch} from 'react-router-dom'
+import './app.css'
 
 import MainPage from './main/main_page'
+import NavBarContainer from './nav/navbar_container'
 
 const App = () => (
+    <div>
+    <NavBarContainer/>
     <Switch>
         <AuthRoute exact path="/" component={MainPage}/>
         {/* <AuthRoute exact path="/login" component={LoginFormContainer}/> */}
         {/* <AuthRoute exact path="/register" component={RegisterFormContainer}/> */}
     </Switch>
+    </div>
 )
 
 export default App
