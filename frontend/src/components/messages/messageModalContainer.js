213e679223718d89887d7f14b1a5f3ccb77258be
Msheezi <marshall.randolph@gmail.com>
2020-04-22 16:41:16 -0700

    Fix logout bug

--- frontend/src/components/messages/messageModalContainer.js
+++ frontend/src/components/messages/messageModalContainer.js
@@ -2,21 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 import MessageModal from "./messageModal";
 import { openModal, closeModal } from "../../actions/ui_actions";
-import {postMessage} from '../../actions/message_actions'
-const mapStateToProps = state => {
-    return {
-        currentUser: state.session.user.id,
-        messageModalOpen: state.ui.messageModalOpen,
-    };
+import { postMessage } from "../../actions/message_actions";
+const mapStateToProps = (state) => {
+  let userId = state.session.user ? state.session.user.id : "";
+  return {
+    currentUser: userId,
+    messageModalOpen: state.ui.messageModalOpen,
+  };
 };
 
-const mapDispatchToProps = dispatch => ({
-    postMessage: (message) => dispatch(postMessage(message)),
-    closeModal: () => dispatch(closeModal()),
-    openModal: () => dispatch(openModal())
+const mapDispatchToProps = (dispatch) => ({
+  postMessage: (message) => dispatch(postMessage(message)),
+  closeModal: () => dispatch(closeModal()),
+  openModal: () => dispatch(openModal()),
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(MessageModal);
+export default connect(mapStateToProps, mapDispatchToProps)(MessageModal);
