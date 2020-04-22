b9191a8325389c2ea6a5e5bc9268b518d48550cb
Msheezi <marshall.randolph@gmail.com>
2020-04-22 16:04:39 -0700

    Predeploy varibles

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ const db = require("./config/keys").mongoURI;
 const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/users");
 const likes = require("./routes/likes");
 const messages = require("./routes/messages");
 const photoUpload = require("./routes/photoUpload");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 mongoose
   .connect(db, {
