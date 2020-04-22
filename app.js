d16f88f2dfba014dbace409195cc83ad1155cb99
Msheezi <marshall.randolph@gmail.com>
2020-04-22 15:38:19 -0700

    Predeployment fixes

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-require('dotenv').config()
+require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
 const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
-const passport = require('passport')
+const passport = require("passport");
 
 const users = require("./routes/users");
 const likes = require("./routes/likes");
-const messages = require("./routes/messages")
-const photoUpload = require("./routes/photoUpload")
+const messages = require("./routes/messages");
+const photoUpload = require("./routes/photoUpload");
 
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false  })
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("Connected to Mongo"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-app.use(passport.initialize())  
-require('./config/passport')(passport)
+app.use(passport.initialize());
+require("./config/passport")(passport);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 app.get("/", (req, res) => {
   res.send("Now You're Cooking with Butter!");
--- routes/messages.js
+++ routes/messages.js
@@ -2,61 +2,42 @@ const express = require("express");
 const router = express.Router();
 const Conversation = require("../models/conversation");
 const Message = require("../models/message");
-const User = require("../models/user")
-
-// test users
-
-// 5e729be9e4afbc3899864aca
-//5e9675e830b5ce3d28676e14
-
-
-// get conversations  this needs to group by conversation
-// find conversations where userID is in participants array,
-// then retrieve messages by conversation
-
+const User = require("../models/user");
 // get list of conversations user is a part of by finding docs with this
 // current users id
- // on load, get a list of all users they have conversations with and store in 
+// on load, get a list of all users they have conversations with and store in
 // redux with the profile pick link and user name, match to users in the messages
 // component, prevents a rough backend creation here
 router.get("/conversations/:userId", (req, res) => {
   let id = req.params.userId;
-  Conversation.find({ "participants": id },{"_id": 1, "participants": 1})
-  .then((convo) => res.json(convo));
-  
-
+  Conversation.find(
+    { participants: id },
+    { _id: 1, participants: 1 }
+  ).then((convo) => res.json(convo));
 });
-
 
 // use conversation ID to get thread details
 // returns array of message objects sorted oldest to newest
 // test data 5e9c91f29e42a21519b4e8d0
 router.get("/conversations/threads/:convoId", (req, res) => {
-    let id = req.params.convoId;
-    Message.find({ "conversationId": id }, {"__v":0}, {
-        sort: { "timeSent": 1 },
-    }).then((threads) => res.json(threads));
+  let id = req.params.convoId;
+  Message.find(
+    { conversationId: id },
+    { __v: 0 },
+    {
+      sort: { timeSent: 1 },
+    }
+  ).then((threads) => res.json(threads));
 });
 
-// router.post("/posttest", (req, res) => {
-//   // let id = req.params.id
-//   // console.log(req.body)
-
-//   let newConversation = new Conversation(req.body);
-//   newConversation.save().then((conversation) => res.json(conversation));
-// });
-
-//get conversation threads
-
 router.post("/postmessage", (req, res) => {
-  //this needs to be a post type
   //find conversation if conversation, save message with ID
   // if no conversation, new Conversation => coversation.id save new message
   let { conversationId, senderId, content } = req.body;
   let userId = req.body.userId;
   let id1 = [req.body.id1, req.body.id2];
   let id2 = [req.body.id2, req.body.id1];
-  
+
   let message = {
     conversationId: conversationId,
     senderId: senderId,
@@ -84,84 +65,44 @@ router.post("/postmessage", (req, res) => {
       // find the conversation with the participants
       // get the conversation id, post the message to correct conversation
       if (result) {
-        Conversation.find({ $or: [{ "participants": id1 }, { "participants": id2 }]}, {"_id": 1})
-        .then(conversation=> {
-          message.conversationId = conversation[0]._id
+        Conversation.find(
+          { $or: [{ participants: id1 }, { participants: id2 }] },
+          { _id: 1 }
+        ).then((conversation) => {
+          message.conversationId = conversation[0]._id;
           newMessage = new Message(message);
-          newMessageconversationId = conversation._id
-        newMessage.save().then((message) => res.json(message));
-        })
-        
+          newMessageconversationId = conversation._id;
+          newMessage.save().then((message) => res.json(message));
+        });
       }
-
     }
   );
-
-  // Conversation.find({ $or:[ {"participants":id1 }, {"participants": id2}]})
-  // .then(conversation=> res.json(conversation))
-  // res.send("this is a test")
 });
-
-
-
-// router.post("/threads/:id", (req, res) => {
-//   // let id = req.params.id
-//   let newMessage = new Message(req.body);
-//   newMessage.save().then((message) => res.json(message));
-// });
-
-
-
-
 
 // use this to load the redux state when going to messages page,
 // this will allow me to get relevant data into the sidebar
 router.get("/messagedUsers/:id", (req, res) => {
-  let currentUser = req.params.id
-  // username, profilephoto, _id 
+  let currentUser = req.params.id;
+  // username, profilephoto, _id
   // { "_id": 1, "username": 1, "mainProfilePic": 1 }
-  const userList = []
-  Conversation.find({ "participants": currentUser }, { "participants": 1 }, function(err, docs){
-    docs.forEach(({ participants }) => {
-      participants.forEach(el => {
-        if (el !== currentUser) {
-          userList.push(el)
-        }
-      })
-    })
-    User.find({ "_id": userList }, { "username": 1, "mainProfilePic": 1,})
-    .then(users => res.json(users))
-  }) 
-    // .then(userListArray => {
-    //   userListArray
-    //   // res.send(userList)console.log(userList)
-    })
-
-
-
-
+  const userList = [];
+  Conversation.find(
+    { participants: currentUser },
+    { participants: 1 },
+    function (err, docs) {
+      docs.forEach(({ participants }) => {
+        participants.forEach((el) => {
+          if (el !== currentUser) {
+            userList.push(el);
+          }
+        });
+      });
+      User.find(
+        { _id: userList },
+        { username: 1, mainProfilePic: 1 }
+      ).then((users) => res.json(users));
+    }
+  );
+});
 
 module.exports = router;
-
-
-
-// router.get("/messagedUsers", (req, res)=>{
-//   let currentUser = req.body.currentUser
-//   // username, profilephoto, _id 
-//   // { "_id": 1, "username": 1, "mainProfilePic": 1 }
-//   const userList = []
-//   Conversation.find({"participants": currentUser}, {"participants": 1} )
-//   .then(userListArray=> {
-//     userListArray.forEach(({participants})=> {
-//       participants.forEach(el => {
-//         if (el !== currentUser){
-//           userList.push(el)
-//         }
-//       })
-//     })
-//     // res.send(userList)console.log(userList)
-//     console.log(userList)
-//     User.find({"_id": userList},{"username": 1, "mainProfilePic": 1, "_id": 1})
-//   })
-//   .then(users => res.json(users))
-// })
