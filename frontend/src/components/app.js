4a1850a4f35303b549a98911bd509be7f49bbe09
Msheezi <marshall.randolph@gmail.com>
2020-09-30 11:55:17 -0700

    Add Analytics

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,6 +3,7 @@ import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import styled from "styled-components";
 import "./app.css";
+import ReactGA from "react-ga";
 
 import MainPage from "./main/main_page";
 import NavBarContainer from "./nav/navbar_container";
@@ -13,8 +14,7 @@ import ProfileContainer from "./profile/profileContainer";
 import PhotoManagerContainer from "./photomanager/photoManagerContainer";
 import ConversationsContainer from "./messages/conversationContainer";
 import ModalContainer from "./messages/messageModalContainer";
-import LikesFeed from './index/likedUsers'
-import ReactGA from 'react-ga'
+import LikesFeed from "./index/likedUsers";
 
 const Footer = styled.footer`
   position: fixed;
@@ -29,35 +29,34 @@ function initializeReactGA() {
   ReactGA.pageview("/homepage");
 }
 
-const App = () => 
-{
-  initializeReactGA()
+const App = () => {
+  initializeReactGA();
 
-return (
-  <div>
-    <NavBarContainer />
-    <ModalContainer />
-    <Switch>
-      <AuthRoute exact path="/" component={MainPage} />
-      <AuthRoute exact path="/login" component={LoginFormContainer} />
-      <AuthRoute exact path="/register" component={SignupFormContainer} />
-      <ProtectedRoute exact path="/home" component={UserFeed} />
-      <ProtectedRoute exact path="/likes" component={LikesFeed} />
-      
-      <ProtectedRoute
-        exact
-        path="/messages"
-        component={ConversationsContainer}
-      />
-      <ProtectedRoute exact path="/users/:id" component={ProfileContainer} />
-      <ProtectedRoute
-        exact
-        path="/photoman"
-        component={PhotoManagerContainer}
-      />
-    </Switch>
-    {/* <Footer>Copyright &copy; 2020 Cuddlr</Footer> */}
-  </div>
-);
-}
+  return (
+    <div>
+      <NavBarContainer />
+      <ModalContainer />
+      <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/register" component={SignupFormContainer} />
+        <ProtectedRoute exact path="/home" component={UserFeed} />
+        <ProtectedRoute exact path="/likes" component={LikesFeed} />
+
+        <ProtectedRoute
+          exact
+          path="/messages"
+          component={ConversationsContainer}
+        />
+        <ProtectedRoute exact path="/users/:id" component={ProfileContainer} />
+        <ProtectedRoute
+          exact
+          path="/photoman"
+          component={PhotoManagerContainer}
+        />
+      </Switch>
+      {/* <Footer>Copyright &copy; 2020 Cuddlr</Footer> */}
+    </div>
+  );
+};
 export default App;
